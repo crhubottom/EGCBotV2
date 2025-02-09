75c8d82d98bd925d5792a786680d964e8bbb6a00
crhubottom <chase.rhubottom@gmail.com>
2025-02-08 18:23:50 -0800

    fixed error

--- src/main/java/com/egc/bot/commands/adminRemove.java
+++ src/main/java/com/egc/bot/commands/adminRemove.java
@@ -14,22 +14,23 @@ public class adminRemove implements ICommand {
     public void run(SlashCommandInteraction ctx) throws SQLException {
         ctx.deferReply().queue();
         if(ctx.getMember().getId().equals("205713886601674753")){
-            if(ctx.getOption("item")!=null&&ctx.getOption("amount")!=null&&ctx.getOption("user")!=null){
-                String item=ctx.getOption("item").getAsString();
-                int amount=ctx.getOption("amount").getAsInt();
-                String user=ctx.getOption("user").getAsString();
-                long receiverID=client.getGuildById(guildID).getMembersByNickname(ctx.getOption("user").getAsString(), true).get(0).getIdLong();
-                inv.DeleteItem(receiverID,item,amount);
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle("Removed "+amount+" "+item+" to "+user);
-            eb.setColor(Color.green);
-            ctx.getHook().sendMessageEmbeds(eb.build()).queue();
+            if(ctx.getOption("item")!=null&&ctx.getOption("amount")!=null&&ctx.getOption("user")!=null) {
+                String item = ctx.getOption("item").getAsString();
+                int amount = ctx.getOption("amount").getAsInt();
+                String user = ctx.getOption("user").getAsString();
+                long receiverID = client.getGuildById(guildID).getMembersByNickname(ctx.getOption("user").getAsString(), true).get(0).getIdLong();
+                inv.DeleteItem(receiverID, item, amount);
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Removed " + amount + " " + item + " to " + user);
+                eb.setColor(Color.green);
+                ctx.getHook().sendMessageEmbeds(eb.build()).queue();
+            }
         }else{
                 EmbedBuilder eb = new EmbedBuilder();
                 eb.setTitle("You are not Chase");
                 eb.setColor(Color.red);
                 ctx.getHook().sendMessageEmbeds(eb.build()).queue();
         }
-        }
     }
 }
+
