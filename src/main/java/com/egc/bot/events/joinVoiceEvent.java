b8debb2f519043fb734d4a752fd362943c503931
crhubottom <chase.rhubottom@gmail.com>
2026-04-09 16:35:47 -0700

    fixed welcome message

--- src/main/java/com/egc/bot/events/joinVoiceEvent.java
+++ src/main/java/com/egc/bot/events/joinVoiceEvent.java
@@ -1,13 +1,13 @@
 package com.egc.bot.events;
 
 import com.egc.bot.AIController;
+import com.egc.bot.audio.PlayerManager;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.io.IOException;
 
-import static com.egc.bot.Bot.AIc;
-import static com.egc.bot.Bot.textModel;
+import static com.egc.bot.Bot.*;
 
 public class joinVoiceEvent extends ListenerAdapter {
     @Override
@@ -23,6 +23,8 @@ public class joinVoiceEvent extends ListenerAdapter {
                     textModel
             );            try {
                 AIc.ttsCall(response, "welcome");
+                PlayerManager playerManager = PlayerManager.get();
+                playerManager.play(client.getGuildById(guildID), "welcome.mp3");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
