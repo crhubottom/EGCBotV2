ad8d4386e8dee355db818ddc979716e608b21a82
crhubottom <chase.rhubottom@gmail.com>
2026-09-14 14:31:39 -0700

    updated pom

--- src/main/java/com/egc/bot/Bot.java
+++ src/main/java/com/egc/bot/Bot.java
@@ -86,11 +86,8 @@ public class Bot {
 
     public Bot() throws InterruptedException, IOException {
         String token = keys.get("DISCORD_KEY");
-        DaveFactory daveFactory = new NativeDaveFactory(); // Using native libdave via jni-impl
 
-        DaveSessionFactory daveSessionFactory = new LDJDADaveSessionFactory(daveFactory);
-        client = JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT).enableCache(CacheFlag.ACTIVITY).enableIntents(GatewayIntent.GUILD_PRESENCES).enableIntents(GatewayIntent.GUILD_MEMBERS).setMemberCachePolicy(MemberCachePolicy.ALL)
-                .setAudioModuleConfig(new AudioModuleConfig().withDaveSessionFactory(daveSessionFactory)).build();
+        client = JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT).enableCache(CacheFlag.ACTIVITY).enableIntents(GatewayIntent.GUILD_PRESENCES).enableIntents(GatewayIntent.GUILD_MEMBERS).setMemberCachePolicy(MemberCachePolicy.ALL).build();
         LoadBalancerRegistry.getDefaultRegistry().register(new PickFirstLoadBalancerProvider());
         new Database();
         currentVoice.add("Random");
